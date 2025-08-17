1dd2b1df97fa588ea271b6d8badfe13d262271a3
MrEcstsy <57694923+MrEcstsy@users.noreply.github.com>
2025-08-17 04:02:41 -0700

    Add files via upload

--- src/ecstsy/AetherisRecode/Loader.php
+++ src/ecstsy/AetherisRecode/Loader.php
@@ -5,44 +5,82 @@ declare(strict_types=1);
 namespace ecstsy\AetherisRecode;
 
 use CameraAPI\CameraHandler;
-use cosmicpe\blockdata\BlockDataFactory;
+use CortexPE\Commando\PacketHooker;
 use cosmicpe\blockdata\world\BlockDataWorldManager;
+use ecstsy\AetherisRecode\blocks\tiles\SpawnerTile;
 use ecstsy\AetherisRecode\commands\BalanceCommand;
+use ecstsy\AetherisRecode\commands\BanCommand;
+use ecstsy\AetherisRecode\commands\BanLookupCommand;
 use ecstsy\AetherisRecode\commands\CoinFlipCommand;
 use ecstsy\AetherisRecode\commands\CollectCommand;
 use ecstsy\AetherisRecode\commands\EcoCommand;
+use ecstsy\AetherisRecode\commands\EnchantmentsCommand;
+use ecstsy\AetherisRecode\commands\EtherealGuardCommand;
+use ecstsy\AetherisRecode\commands\ExpCommand;
+use ecstsy\AetherisRecode\commands\FeedCommand;
+use ecstsy\AetherisRecode\commands\FlyCommand;
+use ecstsy\AetherisRecode\commands\FlySpeedCommand;
+use ecstsy\AetherisRecode\commands\FreezeCommand;
+use ecstsy\AetherisRecode\commands\GamemodeCommand;
+use ecstsy\AetherisRecode\commands\GiveItemCommand;
+use ecstsy\AetherisRecode\commands\HealCommand;
+use ecstsy\AetherisRecode\commands\InfractionsCommand;
+use ecstsy\AetherisRecode\commands\JackpotCommand;
 use ecstsy\AetherisRecode\commands\KitCommand;
 use ecstsy\AetherisRecode\commands\MainMenuCommand;
+use ecstsy\AetherisRecode\commands\SeeBragCommand;
+use ecstsy\AetherisRecode\commands\SeeItemCommand;
 use ecstsy\AetherisRecode\commands\SettingsCommand;
 use ecstsy\AetherisRecode\commands\SkillsCommand;
 use ecstsy\AetherisRecode\commands\SkyBlockCommand;
+use ecstsy\AetherisRecode\commands\SpawnCommand;
 use ecstsy\AetherisRecode\commands\StaffChatCommand;
+use ecstsy\AetherisRecode\commands\TradeCommand;
+use ecstsy\AetherisRecode\commands\UnfreezeCommand;
+use ecstsy\AetherisRecode\commands\WarnCommand;
+use ecstsy\AetherisRecode\enchantments\CustomEnchantments;
 use ecstsy\AetherisRecode\listeners\AetherisListener;
+use ecstsy\AetherisRecode\listeners\AntiCheatListener;
+use ecstsy\AetherisRecode\listeners\CrateListener;
+use ecstsy\AetherisRecode\listeners\EnchantmentListener;
 use ecstsy\AetherisRecode\listeners\ItemListener;
+use ecstsy\AetherisRecode\listeners\RegionListener;
 use ecstsy\AetherisRecode\listeners\SkillsListener;
 use ecstsy\AetherisRecode\listeners\SkyblockListener;
 use ecstsy\AetherisRecode\player\PlayerManager;
-use ecstsy\AetherisRecode\player\skills\SkillManager;
+use ecstsy\AetherisRecode\server\crates\CrateManager;
+use ecstsy\AetherisRecode\server\FloatingTextsInstance;
+use ecstsy\AetherisRecode\server\items\AetherisItem;
+use ecstsy\AetherisRecode\server\items\stardrops\StarDrop;
+use ecstsy\AetherisRecode\server\JackpotInstance;
+use ecstsy\AetherisRecode\server\PunishmentInstance;
+use ecstsy\AetherisRecode\server\regions\RegionManager;
 use ecstsy\AetherisRecode\skyblock\SkyBlockManager;
-use ecstsy\AetherisRecode\spawners\SpawnerData;
+use ecstsy\AetherisRecode\tasks\ItemEntityNameTagUpdateTask;
 use ecstsy\AetherisRecode\utils\QueryStmts;
+use ecstsy\AetherisRecode\utils\SpawnerItem;
 use ecstsy\AetherisRecode\utils\Utils;
 use JackMD\ConfigUpdater\ConfigUpdater;
 use libCustomPack\libCustomPack;
 use muqsit\invmenu\InvMenuHandler;
+use pocketmine\block\tile\TileFactory;
 use pocketmine\plugin\PluginBase;
 use pocketmine\resourcepacks\ZippedResourcePack;
+use pocketmine\Server;
 use pocketmine\utils\SingletonTrait;
 use poggit\libasynql\DataConnector;
 use poggit\libasynql\libasynql;
-use Symfony\Component\Filesystem\Path;
+
+use function PHPSTORM_META\map;
 
 final class Loader extends PluginBase
 {
 
     use SingletonTrait;
 
+    public const SERVER_PREFIX = "&r&l&dETHEREAL&fHUB &r&d► ";
     public const SERVER_TITLE = "&r&3✦ Ethereal Hub &3✦";
+    public const ANTICHEAT_PREFIX = "&r&l&dETHEREAL&fGUARD &r&8► ";
 
     public const PERMISSION_PREFIX = "aetheris.";
 
@@ -56,11 +94,13 @@ final class Loader extends PluginBase
 
     public static SkyBlockManager $skyblockManager;
 
-    public static SkillManager $skillManager;
-
     private BlockDataWorldManager $blockDataWorldManager;
 
     private static ?ZippedResourcePack $resourcePack;
+
+    private static JackpotInstance $jackpotInstance;
+
+    private static PunishmentInstance $punishmentInstance;
 
     protected function onLoad(): void
     {
@@ -71,11 +111,16 @@ final class Loader extends PluginBase
     {
         ConfigUpdater::checkUpdate($this, $this->getConfig(), "version", 1);
 
+        $regionManager = new RegionManager();
         $listeners = [
             new AetherisListener(),
             new SkyblockListener(),
             new ItemListener(),
-            new SkillsListener()
+            new SkillsListener(),
+            new RegionListener($regionManager),
+            new CrateListener(),
+            new EnchantmentListener(),
+            new AntiCheatListener(),
         ];
 
         foreach ($listeners as $listener) {
@@ -85,26 +130,55 @@ final class Loader extends PluginBase
         self::$connector = libasynql::create($this, ["type" => "sqlite", "sqlite" => ["file" => "sqlite.sql"], "worker-limit" => 2], ["sqlite" => "sqlite.sql"]);
         self::$connector->executeGeneric(QueryStmts::PLAYERS_INIT);
         self::$connector->executeGeneric(QueryStmts::ISLANDS_INIT);
-        self::$connector->executeGeneric(QueryStmts::SKILLS_INIT);
         self::$connector->executeGeneric(QueryStmts::COINFLIP_INIT);
+        self::$connector->executeGeneric(QueryStmts::JACKPOT_INIT);
+        self::$connector->executeGeneric(QueryStmts::JACKPOT_STATS_INIT);
+        self::$connector->executeGeneric(QueryStmts::PUNISHMENTS_INIT);
+        self::$connector->executeGeneric(QueryStmts::ACTIVE_PUNISHMENTS_INIT);
+        self::$connector->executeGeneric(QueryStmts::ANTICHEAT_LOGS_INIT);
         self::$connector->waitAll();
 
         self::$playerManager = new PlayerManager($this);
         self::$skyblockManager = new SkyBlockManager($this);
-        self::$skillManager = new SkillManager($this);
+        self::$jackpotInstance = new JackpotInstance();
+        self::$punishmentInstance = new PunishmentInstance();
 
+        $unregisteredCommands = ["gamemode", "ban", "xp"];
+        foreach ($unregisteredCommands as $command) {
+            $this->getServer()->getCommandMap()->unregister(Server::getInstance()->getCommandMap()->getCommand($command));
+        }
 
         $this->getServer()->getCommandMap()->registerAll('Aetheris', [
-            new SkyBlockCommand($this, "island", "View skyblock information", ["is", "skyblock"]),
-            new EcoCommand($this, "economy", "View the economy commands", ['eco']),
-            new BalanceCommand($this, "balance", "View your balance", ['bal']),
-            new MainMenuCommand($this, "menu", "Open the server's main memu"),
-            new KitCommand($this, "kit", "Open the kit menu"),
-            new SkillsCommand($this, "skills", "Open the skills menu"),
-            new CollectCommand($this, "collect", "Open the collect menu", ["collection"]),
-            new SettingsCommand($this, "settings", "Open the settings menu"),
-            new CoinFlipCommand($this, "coinflip", "Open cf menu", ["cf"]),
-            new StaffChatCommand($this, "staffchat", "Toggle the staff chat", ["sc"])
+            new SkyBlockCommand($this, "island", "Manage your SkyBlock island", ["is", "skyblock"]),
+            new EcoCommand($this, "economy", "Economy management commands", ['eco']),
+            new BalanceCommand($this, "balance", "Check your balance", ['bal']),
+            new MainMenuCommand($this, "menu", "Open the main menu"),
+            new KitCommand($this, "kit", "Access the kit menu"),
+            new SkillsCommand($this, "skills", "View your skills"),
+            new CollectCommand($this, "collect", "Open your collection menu", ["collection"]),
+            new SettingsCommand($this, "settings", "Adjust your settings"),
+            new CoinFlipCommand($this, "coinflip", "Play coinflip", ["cf"]),
+            new StaffChatCommand($this, "staffchat", "Toggle the staff chat", ["sc"]),
+            new SeeItemCommand($this, "seeitem", "See the last [item] from a player", ["citem"]),
+            new SeeBragCommand($this, "seebrag", "See a player's last [brag]", ["cbrag"]),
+            new FlySpeedCommand($this, "flyspeed", "Set your fly speed", ["flyspeed"]),
+            new GiveItemCommand($this, "giveitem", "Get a custom aetheris item", ["gi"]),
+            new SpawnCommand($this, "spawn", "Teleport to spawn"),
+            new EnchantmentsCommand($this, "aetherisenchantments", "Enchantments commands", ["ae"]),
+            new GamemodeCommand($this, "gamemode", "Change your gamemode", ["gm", "gmc", "gms", "gma", "gmsp"]),
+            new FlyCommand($this, "fly", "Toggle flight"),
+            new JackpotCommand($this, "jackpot", "View the jackpot", ["jp"]),
+            new FreezeCommand($this, "freeze", "Freeze a player", ["f"]),
+            new UnfreezeCommand($this, "unfreeze", "Unfreeze a player", ["uf"]),
+            new WarnCommand($this, "warn", "Warn a player", ["w"]),
+            new InfractionsCommand($this, "infractions", "View player infractions", ["i"]),
+            new BanCommand($this, "ban", "Ban a player"),
+            new BanLookupCommand($this, "banlookup", "Lookup banned players", ["bl"]),
+            new ExpCommand($this, "exp", "View your exp", ["xp"]),
+            new EtherealGuardCommand($this, "etherealguard", "Anti-cheat commands", ["eg"]),
+            new TradeCommand($this, "trade", "Trade with a player", ["t"]),
+            new FeedCommand($this, "feed", "Restore your hunger"),
+            new HealCommand($this, "heal", "Restore your health"),
         ]);
 
         if (!InvMenuHandler::isRegistered()) {
@@ -115,15 +189,33 @@ final class Loader extends PluginBase
             CameraHandler::register($this);
         }
 
-        libCustomPack::registerResourcePack(self::$resourcePack = libCustomPack::generatePackFromResources($this));
+        if (!PacketHooker::isRegistered()) {
+            PacketHooker::register($this);
+        }
+
+        //libCustomPack::registerResourcePack(self::$resourcePack = libCustomPack::generatePackFromResources($this));
         Utils::initCustomSizedInvMenu();
         $this->getLogger()->info("Resource pack loaded");
 
-        BlockDataFactory::register("AetherisRecode:Spawner", SpawnerData::class);
+        SpawnerItem::getAll();
+        SpawnerItem::initHack();
+        TileFactory::getInstance()->register(SpawnerTile::class, ['MobSpawner', 'minecraft:mob_spawner']);
+        Utils::initDispenserMenu();
+        Utils::registerAetherisEntities();
+        FloatingTextsInstance::register();
+        AetherisItem::init();
+        Utils::initRegions($regionManager);
 
-        $this->blockDataWorldManager = BlockDataWorldManager::create($this);
+        Utils::initCratePositions();
+        Utils::initHandlers();
+        CrateManager::init();
+        StarDrop::init();
+        $this->getScheduler()->scheduleRepeatingTask(new ItemEntityNameTagUpdateTask($this), 20); 
 
-        
+        CustomEnchantments::getAll();
+        Utils::initEnchantHandlers();
+
+        CrateManager::scheduleCrateLetterAnimation();
     }
 
     public function onDisable(): void
@@ -132,10 +224,12 @@ final class Loader extends PluginBase
             self::$connector->close();
         }
 
-        libCustomPack::unregisterResourcePack(self::$resourcePack);
+        //libCustomPack::unregisterResourcePack(self::$resourcePack);
 
         $this->getLogger()->info("resource pack unloaded");
-        unlink(Path::join($this->getDataFolder(), self::$resourcePack->getPackName() . ".mcpack"));
+        //unlink(Path::join($this->getDataFolder(), self::$resourcePack->getPackName() . ".mcpack"));
+
+        FloatingTextsInstance::removeAll();
     }
 
     public static function getDatabase(): DataConnector
@@ -153,13 +247,18 @@ final class Loader extends PluginBase
         return self::$skyblockManager;
     }
 
-    public static function getSkillManager(): SkillManager
-    {
-        return self::$skillManager;
-    }
-
     public static function getBlockDataWorldManager(): BlockDataWorldManager
     {
         return self::$instance->blockDataWorldManager;
+    }
+
+    public static function getJackpotInstance(): JackpotInstance
+    {
+        return self::$jackpotInstance;
+    }
+
+    public static function getPunishmentInstance(): PunishmentInstance
+    {
+        return self::$punishmentInstance;
     }
 }
